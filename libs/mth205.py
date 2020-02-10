0884aa7c0747fc83596ddf4af3fc3142a229ab4c
David Austin <david.austin.m@gmail.com>
2020-02-10 13:12:43 -0500

    vandermonde

--- libs/mth205.py
+++ libs/mth205.py
@@ -274,7 +274,7 @@ def plot_model(xhat, data, color='blue',
     ax.plot(plotx, ploty, color='red')
 
 def vandermonde(data, k):
-    return matrix([ [x**j for j in range(k+1)] for x in data])
+    return matrix([ [x**j for j in range(k+1)] for x in data.entries])
 
 
 
