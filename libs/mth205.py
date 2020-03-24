65521d885cdf36fe1839e1b70401ebf745bebc90
David Austin <david.austin.m@gmail.com>
2020-03-24 06:33:09 -0400

    svd

--- libs/mth205.py
+++ libs/mth205.py
@@ -173,7 +173,7 @@ class matrix:
             fig,ax = plt.subplots(figsize=dims)
         else:
             fig,ax = plt.subplots()
-        ax.set_ylim((0, sv[0]))
+        ax.set_ylim((0, 1.1*sv[0]))
         ax.plot(range(len(sv)), sv, c = color)
         ax.scatter(x = range(len(sv)), y = sv, c = color, s = size)
         
