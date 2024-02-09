ba773ddd7de1494c4ca08aac59d729504c704743
Jagger Denhof <denhofja@mail.gvsu.edu>
2024-02-09 13:05:49 -0500

    stable version with updated name to reflect package

--- setup.py
+++ setup.py
@@ -1,14 +1,9 @@
 from setuptools import find_packages, setup
-"""
-This Pip skeleton was created by following Michael Kim's Pip 
-package tutorial:
-    - https://github.com/MichaelKim0407/tutorial-pip-package
-"""
 
 setup(
-    name="MTH205",
+    name="mth205",
     description="A package extension for sage.",
-    version="0.0.2.dev1",
+    version="0.0.3",
     url="https://github.com/davidaustinm/MTH205-W20",
     author="GVSU MTH205",
     packages=find_packages(),
